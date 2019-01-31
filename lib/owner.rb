de2b61a2e737337bbfd6a482132d224e7e839f44
Kathleen Carrel <katiecarrel@gmail.com>
2019-01-31 20:48:34 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,42 @@
 class Owner
-  # code goes here
+  
+  attr_reader :species
+  attr_accessor :name 
+  
+  @@all = []
+  @@pets = {:cats=>[], :dogs=>[], :fishes=>[]}
+
+  def initialize(species)
+    @species = species 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def self.count 
+    @@all.length 
+  end 
+  
+  def self.reset_all
+   @@all.clear
+  end 
+  
+  def say_species
+    "I am a #{self.species}."
+  end 
+  
+  def name
+    @name 
+  end 
+  
+  def pets 
+    @@pets = {:cats=>[], :dogs=>[], :fishes=>[]}
+  end 
+  
+  def buy_fish 
+    @@pets[:fishes] << Fish.new(fish)
+  end 
+  
 end
